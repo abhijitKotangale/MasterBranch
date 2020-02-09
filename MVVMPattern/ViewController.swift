e88e3f24b2611dba9572f346c03e23f3dabb979e
Abhijit Kotangale <abhijitkotangale@abhijits-macbook-pro-2.local>
2020-02-09 08:16:47 +0530

    Initial Commit

---
+++ MVVMPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMPattern
+//
+//  Created by Abhijit Kotangale on 09/02/20.
+//  Copyright © 2020 Abhijit Kotangale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
